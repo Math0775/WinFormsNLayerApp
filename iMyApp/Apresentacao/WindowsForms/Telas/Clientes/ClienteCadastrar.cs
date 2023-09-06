52572f3b95a16e6e21a3a5c48ebc7d20ca99c551
Matheus <matheusandradepompermayer07@gmail.com>
2023-09-05 21:37:34 -0300

    Implementação de Entidades de Cliente e Colaborador parcialmente.

---
+++ iMyApp/Apresentacao/WindowsForms/Telas/Clientes/ClienteCadastrar.cs
@@ -0,0 +1,24 @@
+﻿using Negocio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForms.Telas.Clientes
+{
+    public partial class ClienteCadastrar : Form
+    {
+        public ClienteCadastrar()
+        {
+            InitializeComponent();
+
+            var cliente = new Cliente();
+        }
+
+    }
+}
